5ff7592a24d7a66e4dc83bb47ceb5e764ec1616e
Ivan Nikolić <niksy5@gmail.com>
2014-12-07 11:37:08 +0100

    Release 0.1.1

--- dist/kist-loader-maps.js
+++ dist/kist-loader-maps.js
@@ -1,4 +1,4 @@
-/*! kist-loader-maps 0.1.0 - Google Maps plugin for kist-loader. | Author: Ivan Nikolić <niksy5@gmail.com> (http://ivannikolic.com/), 2014 | License: MIT */
+/*! kist-loader-maps 0.1.1 - Google Maps plugin for kist-loader. | Author: Ivan Nikolić <niksy5@gmail.com> (http://ivannikolic.com/), 2014 | License: MIT */
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var o;"undefined"!=typeof window?o=window:"undefined"!=typeof global?o=global:"undefined"!=typeof self&&(o=self);var f=o;f=f.jQuery||(f.jQuery={}),f=f.kist||(f.kist={}),f=f.loader||(f.loader={}),f.loadGmaps=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (global){
 var $ = (typeof window !== "undefined" ? window.$ : typeof global !== "undefined" ? global.$ : null);
@@ -35,12 +35,12 @@ function loadGmaps ( options, cb ) {
 
 	loadGoogleMaps(options.mapsVersion, options.apiKey, options.language, options.libraries, options.sensor)
 		.done(function () {
-			var args = [].call(arguments);
+			var args = [].slice.call(arguments);
 			if ( options.plugins ) {
 				loader
 					.load(options.plugins)
 					.done(function () {
-						args.push([].call(arguments));
+						args.push([].slice.call(arguments));
 						success.apply(window, args);
 					})
 					.fail(err);
